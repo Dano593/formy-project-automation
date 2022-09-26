3c32cdac1d352545e1f2c2346f9ce3ea4e9d7974
Dano593 <daniel.armijos.conde@gmail.com>
2022-09-26 14:58:46 -0500

    Initial commit

---
+++ 06_01/start/test/Form.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Form {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Webdrivers\\chrome\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+        driver.findElement(By.id("first-name")).sendKeys("Daniel");
+        driver.findElement(By.id("last-name")).sendKeys("Armijos");
+        driver.findElement(By.id("job-title")).sendKeys("QA");
+        driver.findElement(By.id("radio-button-3")).click();
+        driver.findElement(By.id("checkbox-1")).click();
+        driver.findElement(By.cssSelector("option[value='4']")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("09/22/2022");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        driver.quit();
+    }
+}
